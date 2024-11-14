3b34c99aba5d166a52b26802096dfa3776df568a
Bislon Zulu <bislonzulu1@outlook.com>
2024-11-14 22:42:42 +0200

    task 0.1

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,41 @@
 #!/usr/bin/node
-
-const request = require('request');
-
-if (process.argv.length !== 3) {
-    console.error('Usage: node script_name.js <movie_id>');
-    process.exit(1);
+/**
+ * Wrapper function for request object that allows it
+ * to work with async and await
+ * @param   {String} url - site url
+ * @returns {Promise}    - promise object that resolves
+ *                         with parsed JSON response
+ *                         and rejects with the request error.
+ */
+function makeRequest (url) {
+  const request = require('request');
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) reject(error);
+      else resolve(JSON.parse(body));
+    });
+  });
 }
 
-const movieId = process.argv[2];
+/**
+ * Entry point - makes requests to Star Wars API
+ * for movie info based movie ID passed as a CLI parameter.
+ * Retrieves movie character info then prints their names
+ * in order of appearance in the initial response.
+ */
+async function main () {
+  const args = process.argv;
 
-const url = `https://swapi.dev/api/films/${movieId}/`;
+  if (args.length < 3) return;
 
-request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error fetching film details:', error);
-        return;
-    }
+  const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
+  const movie = await makeRequest(movieUrl);
 
-    if (response.statusCode === 200) {
-        const filmData = JSON.parse(body);
-        const characters = filmData.characters;
+  if (movie.characters === undefined) return;
+  for (const characterUrl of movie.characters) {
+    const character = await makeRequest(characterUrl);
+    console.log(character.name);
+  }
+}
 
-        characters.forEach(characterUrl => {
-            request(characterUrl, (charError, charResponse, charBody) => {
-                if (charError) {
-                    console.error('Error fetching character details:', charError);
-                    return;
-                }
-
-                if (charResponse.statusCode === 200) {
-                    const characterData = JSON.parse(charBody);
-                    console.log(characterData.name);
-                } else {
-                    console.error(`Error fetching character details: ${charResponse.statusCode}`);
-                }
-            });
-        });
-    } else {
-        console.error(`Error fetching film details: ${response.statusCode}`);
-    }
-});
+main();
