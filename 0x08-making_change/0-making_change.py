cda803fbfb926917665d43542a5ed7ade744f17b
Bislon Zulu <bislonzulu1@outlook.com>
2024-12-30 19:34:13 +0200

    modi

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,10 +17,15 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += 1
+            temp += 1
+        if check == total:
+            return temp
+        check -= 1
+        temp -= 1
+    return -1
