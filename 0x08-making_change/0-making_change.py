d5bc83c487641da00a16d1ec60807af635050f69
Bislon Zulu <bislonzulu1@outlook.com>
2024-12-31 20:26:09 +0200

    fixes 2

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,16 +6,12 @@ Module for making change
 
 def makeChange(coins, total):
     """
-    Determine the fewest number of coins needed to meet the given total
-
-    Args:
-    coins (list): List of coin denominations.
-    total (int): Target amount.
-
-    Returns:
-    int: Minimum number of coins neede, or -1 if the total cannont be met.
+    A function to calculate the change.
     """
     def dp(amount, memo):
+        """
+        A dynamic function to break down the problem better.
+        """
         if amount in memo:
             return memo[amount]
         if amount == 0:
@@ -27,11 +23,12 @@ def makeChange(coins, total):
         for coin in coins:
             num_coins = dp(amount - coin, memo) + 1
             min_coins = min(min_coins, num_coins)
-
+        
         memo[amount] = min_coins
         return memo[amount]
-    if total <=0:
+
+    if total <= 0:
         return 0
+    
     result = dp(total, {})
     return result if result != float('inf') else -1
-
